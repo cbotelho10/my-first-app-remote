4799a8707bbba55179891dda4a215889dea9e7f3
chris botelho <chris.botelho29@gmail.com>
2021-09-14 14:13:55 -0400

    my first app! (Kinda)

--- my-first-app/ViewController.swift
+++ my-first-app/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabelISOM: UILabel!
+    @IBOutlet weak var messageLabelUMASS: UILabel!
+    @IBOutlet weak var messageLabelCoding: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func messageButtonISOM(_ sender: UIButton) {
+        messageLabelISOM.text = "ISOM"
+        messageLabelISOM.textColor = .black
+        }
+    
+    @IBAction func messageButtonUMASS(_ sender: UIButton) {
+        messageLabelUMASS.text = "UMASS"
+        
+    }
+    
+    @IBAction func messageButtonCoding(_ sender: UIButton) {
+        messageLabelCoding.text = "I love coding"
+    }
 
 }
 
